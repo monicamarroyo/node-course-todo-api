4ce9bddaaed0451f36693dfba342f5f50117a297
monica <monica.arroyo1319@gmail.com>
2018-04-23 11:49:08 -0500

    Saving changed to mongoose

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect(process.env.MongoDB_URI ||'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI ||'mongodb://localhost:27017/TodoApp');
 // if exists else were going to use the localhost URL making sure it connects to a URl
 
 module.exports = {
